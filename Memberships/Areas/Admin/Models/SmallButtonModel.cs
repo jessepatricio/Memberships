76a22f41b3e278adc0c7524e35eb165c34364c3f
jesse patricio <jessepatricio@gmail.com>
2017-12-15 11:13:24 +1300

    added controllers for Section, Part, ProductType, ItemType

---
+++ Memberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Memberships.Areas.Admin.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        public int? Id { get; set; }
+        public int? ItemId { get; set; }
+        public int? ProductId { get; set; }
+        public int? SubscriptionsId { get; set; }
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder("?");
+                if (Id != null && Id > 0) param.Append($"id={Id}&");
+                if (ItemId != null && ItemId > 0) param.Append($"itemId={ItemId}&");
+                if (ProductId != null && ProductId > 0) param.Append($"productId={ProductId}&");
+                if (SubscriptionsId != null && SubscriptionsId > 0) param.Append($"subscriptionsId={SubscriptionsId}&");
+
+                return param.ToString().Substring(0, param.Length - 1);
+            }
+        }
+
+    }
+}
